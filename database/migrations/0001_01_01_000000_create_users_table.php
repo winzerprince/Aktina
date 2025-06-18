7d780d053abc01691863a3d92e408a881a4189ce
emillypopi <emillyshirley6@gmail.com>
2025-06-18 14:51:17 +0300

    m

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,8 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-        });
+            $table->string('role');
 
+        });
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
