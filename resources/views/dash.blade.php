be0c35a5f97247cea55eaab11b592d1efbda1627
emillypopi <emillyshirley6@gmail.com>
2025-06-18 20:59:05 +0300

    g

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-<h1>This is where the dashboard will be</h1>
+<h1></h1>
 </body>
 </html>
 <!--
@@ -259,17 +259,18 @@
             <div class="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center" style="background-image: url('../assets/img/curved-images/white-curved.jpeg')"></div>
             <div class="relative z-20 flex-auto w-full p-4 text-left text-white">
                 <div class="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
-                    <i class="top-0 z-10 text-transparent ni leading-none ni-diamond text-lg bg-gradient-to-tl from-slate-600 to-slate-300 bg-clip-text opacity-80" sidenav-card-icon></i>
+                    <i class="top-0 z-10 text-transparent ni leading-none ni-diamond text-lg bg-gradient-to-tl from-slate-600 to-slate-300 bg-clip-text opacity-60" sidenav-card-icon></i>
                 </div>
                 <div class="transition-all duration-200 ease-nav-brand">
-                    <h6 class="mb-0 text-white">Need help?</h6>
+                    <h6 class="mb-0 text-white">Need any help?</h6>
                     <p class="mt-0 mb-4 font-semibold leading-tight text-xs">Please check our docs</p>
-                    <a href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/" target="_blank" class="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-xs hover:shadow-soft-2xl hover:scale-102">Documentation</a>
+                    <a href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/" target="_blank" class="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-xs hover:shadow-soft-2xl hover:scale-102">Documents</a>
                 </div>
             </div>
         </div>
         <!-- pro btn  -->
-        <a class="inline-block w-full px-6 py-3 my-4 font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro text-xs bg-gradient-to-tl from-purple-700 to-pink-500 hover:shadow-soft-2xl hover:scale-102" target="_blank" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tailwind?ref=sidebarfree">Upgrade to pro</a>
+       <!-- <a class="inline-block w-full px-6 py-3 my-4 font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro text-xs bg-gradient-to-tl from-green-700 to-yellow-500 hover:shadow-soft-2xl hover:scale-102" target="_blank" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tailwind?ref=sidebarfree">Upgrade to pro</a>-->
+            <a href="#" style="bg-gradient-to-tr from-green-200 to-green-10; color: black; padding: 10px; text-decoration: none; display: inline-block;"class="inline-block w-full px-6 py-3 my-4 font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro text-xs" target="_blank"> upgrade to pro</a>
     </div>
 </aside>
 
