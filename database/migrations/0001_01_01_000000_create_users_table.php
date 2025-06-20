b1750550afc0c67ded515a1b83975b82954b6233
winzerprince <aitajprince200@gmail.com>
2025-06-20 18:08:15 +0300

    added some migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('verified')->default(false);
+            $table->string('company_name')->nullable();
+            $table->json('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->string('role');
---
+++ database/migrations/2025_06_20_123237_create_resource_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resource', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('units')->default(0);
+            $table->integer('reorder_level')->default(100);
+            $table->integer('overstock_level')->default(1000);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resource');
+    }
+};
