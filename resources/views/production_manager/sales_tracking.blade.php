809d5b4f7ed23b8125da289396a068b9933f2a6a
emillypopi <emillyshirley6@gmail.com>
2025-06-23 13:49:04 +0300

    added

--- resources/views/production_manager/sales_tracking.blade.php
+++ resources/views/production_manager/sales_tracking.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.app>
-    <h1 class="text-2xl font-bold mb-4">Production Metrics</h1>
+    <!--<h1 class="text-2xl font-bold mb-4">Sales Tracking</h1>-->
+    <h2 class="text-2xl font-semibold mb-4"> Welcome to the sales tracking view! </h2>
     <p>This is the Sales Tracking page for Production Managers.</p>
 </x-layouts.app>
