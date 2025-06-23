a410a9bb659ff48a49deb0c2a63395734c05791b
JMinallah <jovia.minallahmatata@students.mak.ac.ug>
2025-06-23 12:46:20 +0300

    added the borders to the login page and adaptability to both light and dark mode

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -7,12 +7,12 @@
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                    <span class="flex h-20 w-30 mb-1 items-center justify-center rounded-md">
+                        <x-app-logo-icon class="w-30 h-20 fill-current text-black dark:text-white" />
                     </span>
                     <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
                 </a>
-                <div class="flex flex-col gap-6">
+                <div class="flex flex-col gap-1 -mt-26">
                     {{ $slot }}
                 </div>
             </div>
